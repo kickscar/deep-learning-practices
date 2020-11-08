1d2f00778be71a54a330d1e44a3fe88753d10011
kickscar <kickscar@gmail.com>
2020-11-08 20:33:30 +0900

    로지스틱회귀 - 수치편미분 적용

--- 03.linear-algebra-basics/11/ex04.py
+++ 03.linear-algebra-basics/11/ex04.py
@@ -5,48 +5,63 @@ from matplotlib import pyplot as plt
 
 
 # sigmoid 함수
-def sigmoid(v):
-    return 1 / (1 + np.e ** (-v))
+def sigmoid(x):
+    return 1 / (1 + np.e ** (-x))
+
+
+# loss function 함수
+def loss_func(x, data_x, data_y):
+    e = np.mean([-(dy * np.log(sigmoid(x[0] * dx + x[1])) + (1 - dy) * np.log(1 - sigmoid(x[0] * dx + x[1]))) for dx, dy in tuple(zip(data_x, data_y))])
+    return e
+
+
+def numerical_gradient(f, x, data_l):
+    h = 1e-4
+    gradient = np.zeros_like(x)
+
+    for i in range(x.size):
+        tmp = x[i]
+
+        x[i] = tmp + h
+        h1 = f(x, *data_l)
+
+        x[i] = tmp - h
+        h2 = f(x, *data_l)
+
+        gradient[i] = (h1 - h2) / (2 * h)
+        x[i] = tmp
+
+    return gradient
+
+
+def gradient_descent(f, x, lr=0.01, epoch=100, data_l=None):
+    for i in range(epoch):
+        gradient = numerical_gradient(f, x, data_l)
+        # 출력
+        print(f'epoch={i+1}, gradient={gradient}, x={x}')
+        x -= lr * gradient
+
+    return x
 
 
 # data
-x = [2, 4, 6, 8, 10, 12, 14]
-y = [0, 0, 0, 1, 1, 1, 1]
+times = [2, 4, 6, 8, 10, 12, 14]
+passed = [0, 0, 0, 1, 1, 1, 1]
 
 
-# a, b 값 초기화
-a = 0
-b = 0
-
-# epoch
-# 1 epochs = 10000
-epochs = 60000
-
-# 학습률
-learning_rate = 0.05
-
-# gradient decent algorithm
-for i in range(epochs):
-    for data_x, data_y in tuple(zip(x, y)):
-        diff_a = data_x * (sigmoid(a * data_x + b) - data_y)
-        diff_b = sigmoid(a * data_x + b) - data_y
-
-        # 결과출력
-        print(f'epoch={i + 1}, a={a}, diff_a={diff_a}, b={b}, diff_b={diff_b}')
-
-        # 보정
-        a = a - learning_rate * diff_a
-        b = b - learning_rate * diff_b
+# 경사하강법
+result = gradient_descent(loss_func, np.array([0., 0.]), lr=0.5, epoch=50000, data_l=(times, passed))
+print(result)
 
 # graph
 fig, subplots = plt.subplots(1, 1)
-subplots.scatter(x, y)
+subplots.scatter(times, passed)
 
 x2 = list(np.arange(0, 15, 0.1))
-subplots.plot(x2, [sigmoid(a * value + b) for value in x2])
+subplots.plot(x2, [sigmoid(result[0] * value + result[1]) for value in x2])
 
 plt.show()
 
 
 # predict
-print(sigmoid(a * 7 + b))
+print(sigmoid(result[0] * 7 + result[1]))
