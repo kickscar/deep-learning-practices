834e0e722160903555beae3649db10f9090aef5a
kickscar <kickscar@gmail.com>
2020-11-29 13:20:27 +0900

    수업 예제 순서 바꾸기

--- 04.deep-learning/02.neural-network/07.backpropagation/ex04.py
+++ 04.deep-learning/02.neural-network/07.backpropagation/ex04.py
@@ -1,104 +1,57 @@
-# Backpropagation: Affine + SoftmaxWithLoss Layer Test
+# Backpropagation: Sigmoid Layer Test
+
 import sys
 import os
 from pathlib import Path
 import numpy as np
 try:
     sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
-    from layers import Affine, SoftmaxWithLoss
-    from common import softmax
+    from layers import Sigmoid
     import twolayernet as network
 except ImportError:
     raise ImportError("Library Module Can Not Found")
 
 
-def forward_propagation(x, t=None):
-    for layer in layers:
-        x = layer.forward(x, t) if t is not None and type(layer).__name__ == 'SoftmaxWithLoss' else layer.forward(x)
+# test1 (vector)
+layer = Sigmoid()
 
-    return x
+_x = np.array([0.1, -0.2, 0.3, -0.4, 0.5])
+print(_x)
 
-
-def backward_propagation(dout):
-    for layer in layers[::-1]:
-        dout = layer.backward(dout)
-
-    return dout
+_y = layer.forward(_x)
+print(_y)
+print(layer.out)   # check if out(_y) restored in layer object
 
 
-def loss(x, t):
-    y = forward_propagation(x, t)
-    return y
+_dout = np.array([-0.1, -0.2, -0.3, 0.4, -0.5])
+_dout = layer.backward(_dout)
+print(_dout)
 
 
-def backpropagation_gradient_net(x, t):
-    forward_propagation(x, t)
-    backward_propagation(1)
-
-    gradient = dict()
-
-    for idx, layer in enumerate(layers):
-        if type(layer).__name__ == 'Affine':
-            gradient[f'w{idx+1}'] = layer.dw
-            gradient[f'b{idx+1}'] = layer.db
-
-    return gradient
+print('# =============================================')
 
 
-network.foward_propagation = forward_propagation
-network.loss = loss
+# test2 (matrix) - 형상 주의!
+_x = np.array([
+    [0.1, -0.6, 1.1],
+    [0.2, -0.7, 1.2],
+    [0.3, -0.8, 1.3],
+    [0.4, -0.9, 1.4],
+])
+print(_x)
+
+_y = layer.forward(_x)
+print(_y)
+print(layer.out)   # check if out(_y) restored in layer object
+
+_dout = np.array([
+    [-1.1, 10.6, -2.1],
+    [-1.2, 20.7, -2.2],
+    [-1.3, 30.8, -2.3],
+    [-1.4, 40.9, -2.4],
+])
+_dout = layer.backward(_dout)
+print(_dout)
 
 
-# =================================================================
 
-
-# 1. load training/test data
-_x, _t = np.array([2.6, 3.9, 5.6]), np.array([0, 0, 1])
-
-# 2. hyperparamters
-
-
-# =================================================================
-
-
-# 3. initialize network
-network.initialize(3, 2, 3)
-
-layers = [
-    Affine(network.params['w1'], network.params['b1']),
-    Affine(network.params['w2'], network.params['b2']),
-    SoftmaxWithLoss()
-]
-
-grad = network.numerical_gradient_net(_x, _t)
-print(grad)
-
-network.params['w1'] -= 0.1 * grad['w1']
-network.params['b1'] -= 0.1 * grad['b1']
-network.params['w2'] -= 0.1 * grad['w2']
-network.params['b2'] -= 0.1 * grad['b2']
-loss = network.loss(_x, _t)
-print(f'loss: {loss}')
-
-
-# =================================================================
-
-
-# 3. initialize network
-network.initialize(3, 2, 3)
-
-layers = [
-    Affine(network.params['w1'], network.params['b1']),
-    Affine(network.params['w2'], network.params['b2']),
-    SoftmaxWithLoss()
-]
-
-grad = backpropagation_gradient_net(_x, _t)
-print(grad)
-
-network.params['w1'] -= 0.1 * grad['w1']
-network.params['b1'] -= 0.1 * grad['b1']
-network.params['w2'] -= 0.1 * grad['w2']
-network.params['b2'] -= 0.1 * grad['b2']
-loss = network.loss(_x, _t)
-print(f'loss: {loss}')
--- 04.deep-learning/02.neural-network/07.backpropagation/ex05.py
+++ 04.deep-learning/02.neural-network/07.backpropagation/ex05.py
@@ -1,112 +1,38 @@
-# Backpropagation: Affine(Batch) + SoftmaxWithLoss Layer Test
+# Backpropagation: SoftmaxWithLoss Layer Test
 import sys
 import os
 from pathlib import Path
 import numpy as np
 try:
     sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
-    from layers import Affine, SoftmaxWithLoss
+    from layers import SoftmaxWithLoss
     from common import softmax
     import twolayernet as network
 except ImportError:
     raise ImportError("Library Module Can Not Found")
 
-
-def forward_propagation(x, t=None):
-    for layer in layers:
-        x = layer.forward(x, t) if t is not None and type(layer).__name__ == 'SoftmaxWithLoss' else layer.forward(x)
-
-    return x
-
-
-def backward_propagation(dout):
-    for layer in layers[::-1]:
-        dout = layer.backward(dout)
-
-    return dout
-
-
-def loss(x, t):
-    y = forward_propagation(x, t)
-    return y
-
-
-def backpropagation_gradient_net(x, t):
-    forward_propagation(x, t)
-    backward_propagation(1)
-
-    gradient = dict()
-
-    for idx, layer in enumerate(layers):
-        if type(layer).__name__ == 'Affine':
-            gradient[f'w{idx+1}'] = layer.dw
-            gradient[f'b{idx+1}'] = layer.db
-
-    return gradient
-
-
-network.foward_propagation = forward_propagation
-network.loss = loss
-
-
-# =================================================================
-
-
-# N = 2
-
 # 1. load training/test data
-_x, _t = np.array([
-    [2.6, 3.9, 5.6],
-    [1.76, 0.98, 4.16],
-]), np.array([
-    [0, 0, 1],
-    [1, 0, 0]
-])
+_x, _t = np.array([2.6, 3.9, 5.6]), np.array([0, 0, 1])
 
 # 2. hyperparamters
 
-
-# =================================================================
-
-
 # 3. initialize network
-network.initialize(3, 2, 3)
+layer = SoftmaxWithLoss()
 
-layers = [
-    Affine(network.params['w1'], network.params['b1']),
-    Affine(network.params['w2'], network.params['b2']),
-    SoftmaxWithLoss()
-]
-
-grad = network.numerical_gradient_net(_x, _t)
-print(grad, grad['w1'].shape, grad['b1'].shape, grad['w2'].shape, grad['b2'].shape)
-
-network.params['w1'] -= 0.1 * grad['w1']
-network.params['b1'] -= 0.1 * grad['b1']
-network.params['w2'] -= 0.1 * grad['w2']
-network.params['b2'] -= 0.1 * grad['b2']
-loss = network.loss(_x, _t)
-print(f'loss: {loss}')
+# 4. test
+loss = layer.forward(_x, _t)
+dx = layer.backward()
+print(loss, dx)
 
 
 # =================================================================
 
 
-# 3. initialize network
-network.initialize(3, 2, 3)
+def foward_propagation(x):
+    y = softmax(x)
+    return y
 
-layers = [
-    Affine(network.params['w1'], network.params['b1']),
-    Affine(network.params['w2'], network.params['b2']),
-    SoftmaxWithLoss()
-]
 
-grad = backpropagation_gradient_net(_x, _t)
-print(grad, grad['w1'].shape, grad['b1'].shape, grad['w2'].shape, grad['b2'].shape)
-
-network.params['w1'] -= 0.1 * grad['w1']
-network.params['b1'] -= 0.1 * grad['b1']
-network.params['w2'] -= 0.1 * grad['w2']
-network.params['b2'] -= 0.1 * grad['b2']
+network.foward_propagation = foward_propagation
 loss = network.loss(_x, _t)
-print(f'loss: {loss}')
+print(loss)
