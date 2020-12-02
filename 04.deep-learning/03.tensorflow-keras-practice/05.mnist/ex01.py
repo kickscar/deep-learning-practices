1bad6e400772a9f13c4e9c546227a97e16a64af6
kickscar <kickscar@gmail.com>
2020-12-03 05:02:49 +0900

    mnist - keras model fitting

--- 04.deep-learning/03.tensorflow-keras-practice/05.mnist/ex01.py
+++ 04.deep-learning/03.tensorflow-keras-practice/05.mnist/ex01.py
@@ -1,12 +1,11 @@
 # coding: utf-8
-# MNIST handwritten digit classification model
+# MNIST Handwritten Digit Classification Model
 import sys
 import os
 from pathlib import Path
 import numpy as np
 from matplotlib import pyplot as plt
 from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.python.keras.layers import Dense
 try:
     sys.path.append(os.path.join(Path(os.getcwd()), 'lib'))
@@ -26,13 +25,7 @@ model.add(Dense(10, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
 # 4. model fitting
-history = model.fit(
-    train_x,
-    train_t,
-    validation_data=(test_x, test_t),
-    epochs=30,
-    batch_size=100,
-    verbose=1)
+history = model.fit(train_x, train_t, validation_data=(test_x, test_t), epochs=30, batch_size=100, verbose=1)
 
 # checkpointer = ModelCheckpoint(filepath='./models/{epoch:02d}-{val_loss:.4f}.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
 # early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10)
@@ -47,7 +40,7 @@ history = model.fit(
 
 # 5. training loss
 train_loss = history.history['loss']
-test_loss = history.history['val_loss']     # validation loss(test loss)
+test_loss = history.history['val_loss']
 
 # 6. 그래프로 표현
 x_len = np.arange(len(train_loss))
