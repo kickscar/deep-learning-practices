23c40326e0fee1b3313126288382ea6fe37245d3
kickscar <kickscar@gmail.com>
2020-12-04 09:14:58 +0900

    옵티마이저 그래프 수정

--- 04.deep-learning/02.neural-network/09.parameter-optimizer/ex09.py
+++ 04.deep-learning/02.neural-network/09.parameter-optimizer/ex09.py
@@ -88,10 +88,10 @@ for optimizer_key in optimizers.keys():
     network.layers = []
 
 
+# 3. graph
 def smooth_curve(x):
-    """손실 함수의 그래프를 매끄럽게 하기 위해 사용
-
-    참고：http://glowingpython.blogspot.jp/2012/02/convolution-with-numpy.html
+    """ 그래프를 매끄럽게 하기 위해 사용
+        참고：http://glowingpython.blogspot.jp/2012/02/convolution-with-numpy.html
     """
     window_len = 11
     s = np.r_[x[window_len - 1:0:-1], x, x[-1:-window_len:-1]]
@@ -101,11 +101,12 @@ def smooth_curve(x):
     return y[5:len(y) - 5]
 
 
-# 3. graph
 markers = {"sgd": "o", "momentum": "x", "adagrad": "s", "adam": "D"}
 x = np.arange(iterations)
+
 for key in optimizers.keys():
     plt.plot(x, smooth_curve(train_losses[key]), marker=markers[key], markevery=100, label=key)
+
 plt.xlabel("iterations")
 plt.ylabel("loss")
 plt.ylim(0, 1)
